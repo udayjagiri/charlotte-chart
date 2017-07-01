cb94ce51bba961ae0f5f833a156ded833f3d0c4b
srinivas.sunkari <srinivas.sunkari@zuto.com>
2017-07-01 20:10:37 +0100

    fixed the tests

--- charlotte-chart/web-app.Tests/Controllers/ExpensesControllerTest.cs
+++ charlotte-chart/web-app.Tests/Controllers/ExpensesControllerTest.cs
@@ -1,18 +1,21 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using web_app.Helpers;
+using Assert = NUnit.Framework.Assert;
 
 namespace web_app.Tests.Controllers
 {
+    [TestClass]
     public class ExpensesControllerTest
     {
-        [Test]
+        [TestMethod]
         public void GivenInputIsIntegerThenReturnTrue()
         {
             var result = StringHelper.IsDecimal("10");
             Assert.That(result,Is.True);
         }
 
-        [Test]
+        [TestMethod]
         public void GivenInputIsStringThenReturnFalse()
         {
             var result = StringHelper.IsDecimal("abc");
