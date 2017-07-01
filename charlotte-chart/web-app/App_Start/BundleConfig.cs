582c27312f57015bb530c6611d7d659cfb1da45d
srinivas.sunkari <srinivas.sunkari@zuto.com>
2017-07-01 18:22:51 +0100

    aDDED assets to bundle

--- charlotte-chart/web-app/App_Start/BundleConfig.cs
+++ charlotte-chart/web-app/App_Start/BundleConfig.cs
@@ -7,9 +7,21 @@ namespace web_app
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
+
+
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //                        "~/Scripts/jquery-{version}.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.min.js",
+                "~/Content/vendor/metisMenu/metisMenu.min.js",
+                "~/Content/vendor/raphael/raphael.min.js",
+                "~/Content/vendor/morrisjs/morris.min.js",
+                "~/Content/data/morris-data.js",
+                "~/Content/dist/js/sb-admin-2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,8 +36,13 @@ namespace web_app
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      //                      "~/Content/bootstrap.css",
+                      //                      "~/Content/site.css",
+                      "~/Content/vendor/font-awesome/css/font-awesome.min.css",
+                      "~/Content/vendor/morrisjs/morris.css",
+                      "~/Content/dist/css/sb-admin-2.css",
+                      "~/Content/vendor/metisMenu/metisMenu.min.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css"));
         }
     }
 }
