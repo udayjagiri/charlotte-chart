dbdb9f6a27139c23acab02c231802a579f54f2c4
srinivas.sunkari <srinivas.sunkari@zuto.com>
2017-07-01 21:03:37 +0100

    Added Code to Write to DB

--- charlotte-chart/web-app/Controllers/ExpensesController.cs
+++ charlotte-chart/web-app/Controllers/ExpensesController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.Mvc;
 using web_app.Helpers;
 using web_app.Models;
 
@@ -12,13 +16,45 @@ namespace web_app.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost]
         [AllowAnonymous]
         public ActionResult Index(ExpensesViewModel model)
         {
             if (!StringHelper.IsDecimal(model.Amount))
             {
-                ViewBag.Errors = "Entered Amount is either 0 or not a integer";
+                ViewBag.Errors = "Entered Amount is either 0 or not a integer.";
+            }
+            if (string.IsNullOrEmpty(model.Type))
+            {
+                ViewBag.Errors += " Expense Type is not selected";
+            }
+            else
+            {
+                var connectionString = ConfigurationManager.AppSettings.Get("SQLSERVER_CONNECTION_STRING");
+
+                SqlConnection myConnection = new SqlConnection(connectionString);
+
+                myConnection.Open();
+
+             
+                string dateTime = DateTime.Today.ToShortDateString();
+
+                string query = "INSERT INTO dbo.Expenses (Amount,Type,Date)VALUES (@Amount,@Type,@Date)";
+
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+                myCommand.Parameters.Add("@Amount", model.Amount);
+                myCommand.Parameters.Add("@Type", model.Type);
+                myCommand.Parameters.Add("@Date", dateTime);
+
+                myCommand.ExecuteNonQuery();
+
+                //       myinfo.Text = "connection to db is made";
+                myConnection.Close();
             }
             return View(model);
         }
